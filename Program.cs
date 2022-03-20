04ca558bee0c704aafc830df3e88ab8a30659f14
Ertuğrul Kuyucu <75902272+ertugrulkuyucu@users.noreply.github.com>
2022-03-20 23:16:01 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoderSpaceTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Part 1: add rows and columns to the List.
+            List<List<int>> list = new List<List<int>>();
+            var rand = new Random();
+            for (int i = 0; i < 3; i++)
+            {
+                // Put some integers in the inner lists.
+                List<int> sublist = new List<int>();
+                
+                for (int v = 0; v < 2; v++)
+                {
+                    sublist.Add(rand.Next(1, 5));
+                }
+                // Add the sublist to the top-level List.
+                list.Add(sublist);
+            }
+
+            char x = findLongestSingleSlot(list);
+            Console.WriteLine(x);
+            Console.ReadLine();
+
+        }
+
+
+        public static char findLongestSingleSlot(List<List<int>> leaveTimes)
+        {
+
+            List<int> temp = new List<int>();
+            for (int i = 0; i < leaveTimes.Count; i++)
+            {
+                if (!temp.Contains(leaveTimes[i][0]))
+                {
+                    temp.Add(leaveTimes[i][0]);
+                }
+
+            }
+            int[,] employee = new int[temp.Count, 2];
+
+
+            for (int i = 0; i < temp.Count; i++)
+            {
+                employee[i, 0] = temp[i];
+                int tempTime = 0;
+                for (int x = 0; x < leaveTimes.Count; x++)
+                {
+                    if (leaveTimes[x][0] == temp[i])
+                    {
+                        tempTime += leaveTimes[x][1];
+                    }
+                }
+                employee[i, 1] = tempTime;
+            }
+
+            int maxTime = 0;
+            int employeeID = -1;
+            for (int y = 0; y < employee.GetLength(0); y++)
+            {
+                if (employee[y, 1] > maxTime)
+                {
+                    maxTime = employee[y, 1];
+                    employeeID = employee[y, 0];
+                }
+            }
+
+            return (char)('A' - 1 + employeeID);
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CoderSpaceTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CoderSpaceTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b30d2121-f81e-4b17-82ed-c6ac7f2b247b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
